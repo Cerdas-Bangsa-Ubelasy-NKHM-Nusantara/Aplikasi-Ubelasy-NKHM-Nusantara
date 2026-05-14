3ffd13e857ee9f4497881904158dc3401ca9ec4e
SR.Pakpahan,SST <75168433+SRPakpahanSST@users.noreply.github.com>
2026-05-14 16:38:21 +0700

    Update iq_1.py

--- nkhm/soal/IQ/iq_1.py
+++ nkhm/soal/IQ/iq_1.py
@@ -1,1 +1,9 @@
-
+[
+  {
+    "text": "Siapa presiden pertama Indonesia?",
+    "options": ["Soekarno", "Soeharto", "Habibie", "Megawati"],
+    "correct": "Soekarno",
+    "type": "IQ",
+    "national": false
+  }
+]
