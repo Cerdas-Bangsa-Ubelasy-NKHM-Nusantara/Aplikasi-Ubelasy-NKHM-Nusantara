f1802e0273c2f46945cef12f5a4ff7b9fa2dcfb9
SR.Pakpahan,SST <75168433+SRPakpahanSST@users.noreply.github.com>
2026-05-18 13:57:56 +0700

    Update utils.py

--- nkhm/utils.py
+++ nkhm/utils.py
@@ -1,17 +1,30 @@
 # nkhm/utils.py
-def calculate_nkhm(iq, eq, sq, aq):
+
+def calculate_nkhm_q(iq, eq, sq, aq):
+    """
+    Menghitung NKHM_Q berdasarkan 4 kecerdasan.
+    Rumus: ((IQ+EQ) × (SQ+AQ)) / ((IQ+EQ) + (SQ+AQ))
+    """
     pembilang = (iq + eq) * (sq + aq)
     penyebut = (iq + eq) + (sq + aq)
     if penyebut == 0:
         return 0
     return round(pembilang / penyebut, 2)
 
-def get_nkhm_level(nkhm):
-    if nkhm >= 80:
+def calculate_nkhm_total(nkhm_q, nasionalisme):
+    """
+    Menghitung NKHM_Total berdasarkan NKHM_Q dan skor Nasionalisme.
+    Rumus: (NKHM_Q + Nasionalisme) / 2
+    """
+    return round((nkhm_q + nasionalisme) / 2, 2)
+
+def get_nkhm_level(nkhm_total):
+    if nkhm_total >= 80:
         return "🌟 Pahlawan Cerdas", "green"
-    elif nkhm >= 60:
+    elif nkhm_total >= 60:
         return "📚 Cendekia Muda", "blue"
-    elif nkhm >= 40:
+    elif nkhm_total >= 40:
         return "🌱 Penjelajah Ilmu", "orange"
     else:
         return "🌿 Perintis Jalan", "gray"
+        
