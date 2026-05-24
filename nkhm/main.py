f7cdf1606745472f3b6c166287e7fd2c9d221ed8
SR.Pakpahan,SST <75168433+SRPakpahanSST@users.noreply.github.com>
2026-05-25 06:35:31 +0700

    Update main.py

--- nkhm/main.py
+++ nkhm/main.py
@@ -101,7 +101,7 @@ def main():
         st.empty()
         col1, col2, col3 = st.columns([1, 2, 1])
         with col2:
-            logo_url = "https://raw.githubusercontent.com/Cerdas-Bangsa-Ubelasy-NKHM-Nusantara/Aplikasi-Ubelasy-NKHM-Nusantara/8785f300fb10ede15e1ade31af699a964938e9ff/assets/garuda.jpg"
+            logo_url = "https://raw.githubusercontent.com/Cerdas-Bangsa-Ubelasy-NKHM-Nusantara/Aplikasi-Ubelasy-NKHM-Nusantara/db45eadb45891c0c3f3028e9be822dc0a85d3346/assets/garuda.jpg"
             st.markdown(f'<div style="display: flex; justify-content: center;"><img src="{logo_url}" width="300"></div>', unsafe_allow_html=True)
             st.markdown("<h1 style='text-align: center;'>🌿 NKHM Nusantara</h1>", unsafe_allow_html=True)
             st.markdown("<p style='text-align: center; font-size: 18px;'>Aplikasi gaming 4 Kecerdasan (IQ, EQ, SQ, AQ) + Nasionalisme<br>Berbasis Perkembangan Data Personal</p>", unsafe_allow_html=True)
