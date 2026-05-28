695d84a64a2c903a60f260467a26f793a06192cb
SR.Pakpahan,SST <75168433+SRPakpahanSST@users.noreply.github.com>
2026-05-29 01:45:02 +0700

    Update utils.py

--- nkhm/utils.py
+++ nkhm/utils.py
@@ -1,1 +1,10 @@
 
+def calculate_nkhm_q(iq, eq, sq, aq):
+    pembilang = (iq + eq) * (sq + aq)
+    penyebut = (iq + eq) + (sq + aq)
+    if penyebut == 0:
+        return 0
+    return round(pembilang / penyebut, 2)
+
+def calculate_nkhm_total(nkhm_q, nasionalisme):
+    return round((nkhm_q + nasionalisme) / 2, 2)
