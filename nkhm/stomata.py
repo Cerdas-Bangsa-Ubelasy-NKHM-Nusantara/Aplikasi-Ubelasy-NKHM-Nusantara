408848f4d6ef7e82522db05f2ce754c8cb7642b7
SR.Pakpahan,SST <75168433+SRPakpahanSST@users.noreply.github.com>
2026-06-09 08:44:34 +0700

    Update stomata.py

--- nkhm/stomata.py
+++ nkhm/stomata.py
@@ -2,11 +2,14 @@
 import streamlit as st
 from nkhm.stomata_tanggapan import show_tanggapan
 from nkhm.stomata_pilihan_benar_salah import show_pilihan_benar_salah
+from nkhm.stomata_pilihan_ganda import show_pilihan_ganda
 
 def show_stomata():
     st.markdown("## 💖 Sto-mata Hati")
-    tab1, tab2 = st.tabs(["📝 Tanggapan (Skala Likert)", "✅ Pilihan Benar/Salah"])
+    tab1, tab2, tab3 = st.tabs(["📝 Tanggapan (Skala Likert)", "✅ Pilihan Benar/Salah", "🔢 Pilihan Ganda (a,b,c,d)"])
     with tab1:
         show_tanggapan()
     with tab2:
         show_pilihan_benar_salah()
+    with tab3:
+        show_pilihan_ganda()
