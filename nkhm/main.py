2a78ca01cfa9fa24618e00fe167d37789b1763bd
SR.Pakpahan,SST <75168433+SRPakpahanSST@users.noreply.github.com>
2026-06-23 21:55:12 +0700

    Update main.py

--- nkhm/main.py
+++ nkhm/main.py
@@ -198,14 +198,7 @@ def main():
         
     # ========== TAB 1: KUIS ==========
     with tab1:
-        # ========== GANTI DENGAN VIDEO ==========
-        video_path = Path(__file__).parent.parent / "assets" / "kuis.mp4"
-        if video_path.exists():
-            # Baca file video sebagai bytes
-            with open(video_path, "rb") as f:
-                video_bytes = f.read()
-            st.video(video_bytes, loop=True, autoplay=True)
-        
+             
         img_path = Path(__file__).parent.parent / "assets" / "kuis.gif"
         if img_path.exists():
             st.image(str(img_path), caption="Asah 4 Kecerdasan dan Nasionalisme 🇮🇩", use_container_width=True)
