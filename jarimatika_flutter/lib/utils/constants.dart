9b3040f8e7f595f3db018de252ee9e4057195f9e
SR.Pakpahan,SST <75168433+SRPakpahanSST@users.noreply.github.com>
2026-07-07 12:08:04 +0700

    Update constants.dart

--- jarimatika_flutter/lib/utils/constants.dart
+++ jarimatika_flutter/lib/utils/constants.dart
@@ -1,1 +1,17 @@
-nnn
+class AppConstants {
+  static const String appName = '🖐️ Jarimatika';
+  static const String modelPath = 'assets/models/hand_landmark.tflite';
+  
+  // Mapping jari ke angka (Jarimatika PMD)
+  static const List<String> fingerNames = [
+    'Kelingking', // 6
+    'Manis',      // 7
+    'Tengah',     // 8
+    'Telunjuk',   // 9
+    'Jempol',     // 10
+  ];
+  
+  static const int minNumber = 6;
+  static const int maxNumber = 10;
+  static const int totalFingers = 5;
+}
