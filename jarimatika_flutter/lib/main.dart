4967a4ee8d2d48278920fbc6236ffe643e209bb4
SR.Pakpahan,SST <75168433+SRPakpahanSST@users.noreply.github.com>
2026-07-07 12:14:13 +0700

    Update main.dart

--- jarimatika_flutter/lib/main.dart
+++ jarimatika_flutter/lib/main.dart
@@ -1,1 +1,24 @@
-nnn
+import 'package:flutter/material.dart';
+import 'screens/jarimatika_screen.dart';
+
+void main() {
+  runApp(const JarimatikaApp());
+}
+
+class JarimatikaApp extends StatelessWidget {
+  const JarimatikaApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Jarimatika App',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        fontFamily: 'Quicksand',
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const JarimatikaScreen(),
+    );
+  }
+}
