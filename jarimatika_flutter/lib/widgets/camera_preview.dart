51f193d2041d9403b58c47ba38e5cb9bf71ca870
SR.Pakpahan,SST <75168433+SRPakpahanSST@users.noreply.github.com>
2026-07-07 12:10:46 +0700

    Update camera_preview.dart

--- jarimatika_flutter/lib/widgets/camera_preview.dart
+++ jarimatika_flutter/lib/widgets/camera_preview.dart
@@ -1,1 +1,55 @@
-nnn
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+class CameraPreviewWidget extends StatelessWidget {
+  final CameraController? controller;
+  final bool isReady;
+  final VoidCallback? onCameraReady;
+
+  const CameraPreviewWidget({
+    Key? key,
+    this.controller,
+    this.isReady = false,
+    this.onCameraReady,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: isReady && controller != null
+            ? CameraPreview(controller!)
+            : _buildLoadingIndicator(),
+      ),
+    );
+  }
+
+  Widget _buildLoadingIndicator() {
+    return Container(
+      width: double.infinity,
+      height: 300,
+      color: Colors.grey[900],
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircularProgressIndicator(
+              color: Colors.white,
+              strokeWidth: 2,
+            ),
+            const SizedBox(height: 12),
+            Text(
+              'Menunggu kamera...',
+              style: TextStyle(color: Colors.grey[400], fontSize: 14),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
